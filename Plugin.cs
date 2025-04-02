e0471295a9499a790c3011ba0e4a20d2c89eac09
RemuSalminen <remumooses.salminen@gmail.com>
2025-04-02 17:28:20 +0300

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using System.Reflection;
+
+namespace PlayCount;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log = base.Log;
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        Log.LogInfo($"Loaded {MyPluginInfo.PLUGIN_NAME}!");
+    }
+}
