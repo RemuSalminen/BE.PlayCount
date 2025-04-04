b56b06d6661c6132caa71cb6936e597097fb23a5
RemuSalminen <remumooses.salminen@gmail.com>
2025-04-04 17:41:32 +0300

    Retrieve the opponents data

--- Patches/MultiSteps.cs
+++ Patches/MultiSteps.cs
@@ -5,15 +5,17 @@ namespace PlayCount.Patches;
 [HarmonyPatch]
 public class MultiSteps
 {
+    // Game start logic
     [HarmonyPostfix]
     [HarmonyPatch(typeof(MultiStartGameMenuStep), nameof(MultiStartGameMenuStep.Open))]
-    public static void BannerOpenFix()
+    public static void BannerOpenFix(MultiStartGameMenuStep __instance)
     {
         Plugin.Log.LogInfo("Opened Multiplayer UI!");
         Plugin.isActive = true;
 
         Plugin.Log.LogInfo("Fetching Stats...");
-        (int CandleAmount, int Wins, int Losses, bool WonPrevious, bool New) = Internal.Stats.GetStats();
+        string CurOpponentID = __instance._opponentBanner.Data.PlayFabID;
+        (int CandleAmount, int Wins, int Losses, bool WonPrevious, bool New) = Internal.Stats.GetStats(CurOpponentID);
         Plugin.Log.LogInfo($"Fetch Complete!\n ShowScore: {Plugin.ShowScore.Value}\n W: {Wins}\n L: {Losses}\n Prev: {WonPrevious}");
 
         Plugin.Log.LogInfo($"Placing {CandleAmount} Candles!");
@@ -36,5 +38,16 @@ public class MultiSteps
         Plugin.Log.LogInfo("Clearing Candles!");
         Internal.CandleSystem.Clear();
         Plugin.Log.LogInfo("Cleared Candles!");
+    }
+
+
+    // Game end & data update logic
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(RankedEndGameMenuStep), nameof(RankedEndGameMenuStep.Open))]
+    public static void EndGameFix(RankedEndGameMenuStep __instance)
+    {
+        Plugin.Log.LogInfo("Ranked Match Over!");
+        UI.EndGameScore.PlayerData Opponent = __instance._endGameScoreUI.RightPlayer;
+        Internal.Stats.UpdateStats(Opponent);
     }
 }
