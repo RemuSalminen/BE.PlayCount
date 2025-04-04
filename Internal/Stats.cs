ba5ece3a3c642ca67f660525c119e5e05c5f8e6c
RemuSalminen <remumooses.salminen@gmail.com>
2025-04-04 17:43:10 +0300

    Give Stats more accurate naming

--- Internal/Stats.cs
+++ Internal/Stats.cs
@@ -10,12 +10,20 @@ namespace PlayCount.Internal;
 public class Stats
 {
     static readonly int CandleAmount = Plugin.ShowScore.Value ? 2 : 1;
-    static int Wins;
-    static int Losses;
-    static bool WonPrevious;
-    static bool New;
-    public static (int CandleAmount, int Wins, int Losses, bool WonPrevious, bool New) GetStats()
+    static int Wins = 0;
+    static int Losses = 0;
+    /// <summary>
+    /// Did the Player win their previous match
+    /// </summary>
+    static bool WonPrevious = false;
+    static bool New = true;
+    static string LastOpponentID = null;
+    static string CurOpponentID = null;
+    public static (int CandleAmount, int Wins, int Losses, bool WonPrevious, bool New) GetStats(string curOpponentID)
     {
+        CurOpponentID = curOpponentID;
+        if (CurOpponentID == LastOpponentID) return (CandleAmount, Wins, Losses, WonPrevious, New);
+
         Wins = 2;
         Losses = 3;
         WonPrevious = false;
