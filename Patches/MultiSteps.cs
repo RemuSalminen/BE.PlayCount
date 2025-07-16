e05f28635ee53d072a893f7994e744c861f02bcb
RemuSalminen <remumooses.salminen@gmail.com>
2025-07-16 15:59:18 +0300

    Convert MultiStep.cs

--- Patches/MultiSteps.cs
+++ Patches/MultiSteps.cs
@@ -10,23 +10,23 @@ public class MultiSteps
     [HarmonyPatch(typeof(MultiStartGameMenuStep), nameof(MultiStartGameMenuStep.Open))]
     public static void BannerOpenFix(MultiStartGameMenuStep __instance)
     {
-        Plugin.Log.LogDebug("Multi: Opened Multiplayer UI!");
+        Plugin.Log.Msg("DEBUG: Multi: Opened Multiplayer UI!");
         Plugin.isActive = true;
 
-        Plugin.Log.LogInfo("Multi: Fetching Stats...");
-        if (__instance == null) { Plugin.Log.LogFatal("Opponent Does Not Exist!"); return; }
+        Plugin.Log.Msg("Multi: Fetching Stats...");
+        if (__instance == null) { Plugin.Log.BigError("Opponent Does Not Exist!"); return; }
         string CurOpponentID = __instance._opponentBanner.Data.PlayFabID;
         (int CandleAmount, int Wins, int Losses, bool WonPrevious, bool New) = Internal.Stats.GetStats(CurOpponentID);
-        Plugin.Log.LogInfo($"Multi: Fetch Complete!");
-        Plugin.Log.LogDebug($"Data:\n ShowScore: {Plugin.ShowScore.Value}\nNew: {New}\n W: {Wins}\n L: {Losses}\n Prev: {WonPrevious}");
+        Plugin.Log.Msg($"Multi: Fetch Complete!");
+        Plugin.Log.Msg($"DEBUG: Data:\n ShowScore: {Plugin.ShowScore.Value}\nNew: {New}\n W: {Wins}\n L: {Losses}\n Prev: {WonPrevious}");
 
-        Plugin.Log.LogInfo($"Multi: Placing {CandleAmount} Candle(s)!");
+        Plugin.Log.Msg($"Multi: Placing {CandleAmount} Candle(s)!");
         Internal.CandleSystem.Place(CandleAmount, Wins, Losses);
-        Plugin.Log.LogInfo("Multi: Candles Placed!");
+        Plugin.Log.Msg("Multi: Candles Placed!");
 
-        Plugin.Log.LogInfo("Multi: Lighting Candles!");
+        Plugin.Log.Msg("Multi: Lighting Candles!");
         Internal.CandleSystem.DecorateCandles(WonPrevious, New);
-        Plugin.Log.LogInfo("Multi: Candles Lit!");
+        Plugin.Log.Msg("Multi: Candles Lit!");
     }
 
     [HarmonyPrefix]
@@ -35,11 +35,11 @@ public class MultiSteps
     {
         if (!Plugin.isActive) return;
         Plugin.isActive = false;
-        Plugin.Log.LogDebug("Multi: Closing Multiplayer UI Soon!");
+        Plugin.Log.Msg("DEBUG: Multi: Closing Multiplayer UI Soon!");
 
-        Plugin.Log.LogInfo("Multi: Clearing Candles!");
+        Plugin.Log.Msg("Multi: Clearing Candles!");
         Internal.CandleSystem.Clear();
-        Plugin.Log.LogInfo("Multi: Cleared Candles!");
+        Plugin.Log.Msg("Multi: Cleared Candles!");
     }
 
 
@@ -48,7 +48,7 @@ public class MultiSteps
     [HarmonyPatch(typeof(RankedEndGameMenuStep), nameof(RankedEndGameMenuStep.Open))]
     public static void EndGameFix(RankedEndGameMenuStep __instance)
     {
-        Plugin.Log.LogDebug("Multi: Ranked Match Over!");
+        Plugin.Log.Msg("DEBUG: Multi: Ranked Match Over!");
         UI.EndGameScore.PlayerData Opponent = __instance._endGameScoreUI.RightPlayer;
         Internal.Stats.UpdateStats(Opponent);
     }
