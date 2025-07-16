d611033185f93a07a35d2803ddaad6f876674ae8
RemuSalminen <remumooses.salminen@gmail.com>
2025-07-16 16:03:12 +0300

    Convert CandleDebug

--- Patches/CandleDebug.cs
+++ Patches/CandleDebug.cs
@@ -17,14 +17,14 @@ public class CandleDebug
     [HarmonyPatch(typeof(CandleActivation), nameof(CandleActivation.ActivateCandle))]
     public static void CActivate()
     {
-        Plugin.Log.LogDebug("Activated Candle!");
+        Plugin.Log.Msg("TEST: Activated Candle!");
     }
     //
     [HarmonyPostfix]
     [HarmonyPatch(typeof(CandleActivation), nameof(CandleActivation.SetActiveCandle))]
     public static void CActive()
     {
-        Plugin.Log.LogDebug("Set Active Candle!");
+        Plugin.Log.Msg("TEST: Set Active Candle!");
     }
 
 
@@ -33,14 +33,14 @@ public class CandleDebug
     [HarmonyPatch(typeof(MenuCandle), nameof(MenuCandle.Start))]
     public static void CStart()
     {
-        Plugin.Log.LogDebug("Started MenuCandle!");
+        Plugin.Log.Msg("TEST: Started MenuCandle!");
     }
     // Called when candle has been cut
     [HarmonyPostfix]
     [HarmonyPatch(typeof(MenuCandle), nameof(MenuCandle.Cut))]
     public static void CCut()
     {
-        Plugin.Log.LogDebug("Cut MenuCandle!");
+        Plugin.Log.Msg("TEST: Cut MenuCandle!");
     }
 
     // 
@@ -48,28 +48,28 @@ public class CandleDebug
     [HarmonyPatch(typeof(CandleBase), nameof(CandleBase.PlayCandleCut))]
     public static void PCCut()
     {
-        Plugin.Log.LogDebug("Played MenuCandle Cut!");
+        Plugin.Log.Msg("TEST: Played MenuCandle Cut!");
     }
     // Continuosly called when a candle is burning
     [HarmonyPostfix]
     [HarmonyPatch(typeof(CandleBase), nameof(CandleBase.StartBurning))]
     public static void CSBurning()
     {
-        Plugin.Log.LogDebug("MenuCandle Started Burning!");
+        Plugin.Log.Msg("TEST: MenuCandle Started Burning!");
     }
     //
     [HarmonyPostfix]
     [HarmonyPatch(typeof(CandleBase), nameof(CandleBase.StopBurning))]
     public static void CStopBurning()
     {
-        Plugin.Log.LogDebug("MenuCandle Stopped Burning!");
+        Plugin.Log.Msg("TEST: MenuCandle Stopped Burning!");
     }
     // Called when transitioning to a new menu via candle cut
     [HarmonyPostfix]
     [HarmonyPatch(typeof(CandleBase), nameof(CandleBase.OnTriggerEnter))]
     public static void CTriggerEnter()
     {
-        Plugin.Log.LogDebug("Entered MenuCandle Trigger!");
+        Plugin.Log.Msg("TEST: Entered MenuCandle Trigger!");
     }
 }
 #endif
